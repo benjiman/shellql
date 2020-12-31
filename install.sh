c5cd6e69904d158596e0af530d80bc3177aaeb19
benji <git@benjiweber.com>
2020-12-31 12:47:02 +0000

    Create flag to skip tests if needed

--- install.sh
+++ install.sh
@@ -8,5 +8,11 @@ run_tests() {
     popd
 }
 
-run_tests
-install "$HOME/bin"
+if [[ "$*" =~ .*skip-tests.* ]]; then
+    echo "Skipping tests"
+else
+    run_tests
+fi
+
+echo "Installing to ${HOME}/bin"
+install "${HOME}/bin"
